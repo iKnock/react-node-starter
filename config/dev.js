9222a57b5c8cbc86e859e9233ad79ba576aac58a
iKnock <ergana.henok@gmail.com>
2021-03-04 19:48:45 +0100

    update cache code

--- config/dev.js
+++ config/dev.js
@@ -3,7 +3,7 @@ module.exports = {
     '70265989829-0t7m7ce5crs6scqd3t0t6g7pv83ncaii.apps.googleusercontent.com',
   googleClientSecret: '8mkniDQOqacXtlRD3gA4n2az',
   mongoURI: 'mongodb://localhost:27017/course-project',
-  //mongoURI: 'mongodb://iknock79:<myuserrr@22>@iknockblogster-shard-00-00.hlyz7.mongodb.net:27017,iknockblogster-shard-00-01.hlyz7.mongodb.net:27017,iknockblogster-shard-00-02.hlyz7.mongodb.net:27017/<dbname>?ssl=true&replicaSet=atlas-s4g73p-shard-0&authSource=admin&retryWrites=true&w=majority',
+  //mongoURI: 'r-shard-00-00.hlyz7.mongodb.net:27017,iknockblogster-shard-00-01.hlyz7.mongodb.net:27017,iknockblogster-shard-00-02.hlyz7.mongodb.net:27017/<dbname>?ssl=true&replicaSet=atlas-s4g73p-shard-0&authSource=admin&retryWrites=true&w=majority',
   cookieKey: '123123123',
 };
 

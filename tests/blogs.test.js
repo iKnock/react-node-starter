1d6a038d89394090b5133b8452640ca20c28f4ce
iKnock <ergana.henok@gmail.com>
2021-04-24 17:09:44 +0200

    adding describe blog testing validation

--- tests/blogs.test.js
+++ tests/blogs.test.js
@@ -11,10 +11,29 @@ afterEach(async () => {
     await page.close();
 });
 
-test('when logged in, must see blog creating form', async () => {
-    await page.login();
-    await page.click('a.btn-floating');
-    const label = await page.getContentsOf('form label');
+//describe allow as to creates a block that groups together several related tests
+describe('When logged in', () => {
+    beforeEach(async () => {
+        await page.login();
+        await page.click('a.btn-floating')
+    });
 
-    expect(label).toEqual('Blog Title');
+    test('Must see blog creating form', async () => {
+        const label = await page.getContentsOf('form label');
+        expect(label).toEqual('Blog Title');
+    })
+
+    describe('And using invalid inputs', () => {
+        beforeEach(async () => {
+            await page.click('form button');
+        });
+
+        test('the form shows an error message', async () => {
+            const titleError = await page.getContentsOf('.title .red-text');
+            const contentError = await page.getContentsOf('.content .red-text');
+
+            expect(titleError).toEqual('You must provide a value');
+            expect(contentError).toEqual('You must provide a value');
+        })
+    })
 })

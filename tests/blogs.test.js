b6435e94790a56fd18d56ed7c23e65a9d83ee18b
iKnock <ergana.henok@gmail.com>
2021-04-26 16:38:32 +0200

    test not logged in user request

--- tests/blogs.test.js
+++ tests/blogs.test.js
@@ -62,3 +62,34 @@ describe('When logged in', () => {
         })
     })
 })
+
+describe('User is not logged in', () => {
+    test('User cannot create blog posts', async () => {
+        const result = await page.evaluate(() => {
+            return fetch('/api/blogs', {
+                method: 'POST',
+                credentials: 'same-origin',
+                headers: {
+                    'Content-Type': 'application/json'
+                },
+                body: JSON.stringify({ title: 'My Title', content: 'My Content' })
+            }).then(res => res.json());
+        })
+
+        expect(result).toEqual({ error: 'You must log in!' });
+    })
+
+    test('User cannot get a list of posts', async () => {
+        const result = await page.evaluate(() => {
+            return fetch('/api/blogs', {
+                method: 'GET',
+                credentials: 'same-origin',
+                headers: {
+                    'Content-Type': 'application/json'
+                }
+            }).then(res => res.json());
+        })
+
+        expect(result).toEqual({ error: 'You must log in!' })
+    })
+})

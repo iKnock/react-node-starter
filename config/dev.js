61a3c5f5650ee9daed19c7c323a01e700370cb03
iKnock <ergana.henok@gmail.com>
2021-04-26 18:15:47 +0200

    config ci

--- config/dev.js
+++ config/dev.js
@@ -1,6 +1,5 @@
 module.exports = {
-  googleClientID:
-    '70265989829-0t7m7ce5crs6scqd3t0t6g7pv83ncaii.apps.googleusercontent.com',
+  googleClientID: '70265989829-0t7m7ce5crs6scqd3t0t6g7pv83ncaii.apps.googleusercontent.com',
   googleClientSecret: '8mkniDQOqacXtlRD3gA4n2az',
   mongoURI: 'mongodb://localhost:27017/course-project',
   //mongoURI: 'r-shard-00-00.hlyz7.mongodb.net:27017,iknockblogster-shard-00-01.hlyz7.mongodb.net:27017,iknockblogster-shard-00-02.hlyz7.mongodb.net:27017/<dbname>?ssl=true&replicaSet=atlas-s4g73p-shard-0&authSource=admin&retryWrites=true&w=majority',
